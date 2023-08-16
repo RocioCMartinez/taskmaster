1f8e0a387da023ee2a4ba5815e1f2d8480e6ebc1
RocioCMartinez <rym5411@gmail.com>
2023-08-15 20:52:47 -0400

    added task detailsactivity UIs

--- app/src/main/java/com/rocio/taskmaster/MainActivity.java
+++ app/src/main/java/com/rocio/taskmaster/MainActivity.java
@@ -1,32 +1,61 @@
 package com.rocio.taskmaster;
 
+import static com.rocio.taskmaster.activities.UserProfileActivity.USERNAME_TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.rocio.taskmaster.activities.AddTaskActivity;
 import com.rocio.taskmaster.activities.AllTasksActivity;
+import com.rocio.taskmaster.activities.TaskDetailsActivity;
 import com.rocio.taskmaster.activities.UserProfileActivity;
 
 public class MainActivity extends AppCompatActivity {
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
         setUpUserProfileButton();
+        setUpTaskOneButton();
         setUpAddTaskFormButton();
         setUpAllTasksButton();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        setupUsernameTextView();
     }
 
     void setUpUserProfileButton(){
         ((ImageView)findViewById(R.id.MainActivityUserProfileButton)).setOnClickListener(view -> {
             Intent goToUserProfileActivityIntent = new Intent(MainActivity.this, UserProfileActivity.class);
             startActivity(goToUserProfileActivityIntent);
+        });
+    }
+
+    void setupUsernameTextView(){
+        String userName = preferences.getString(USERNAME_TAG, "No username");
+        ((TextView)findViewById(R.id.MainActivityUserName)).setText(userName + "'s Tasks");
+    }
+
+    void setUpTaskOneButton() {
+        ((Button)findViewById(R.id.MainActivityTaskOneBtn)).setOnClickListener(view -> {
+            Intent goToTaskDetails = new Intent(MainActivity.this, TaskDetailsActivity.class);
+            startActivity(goToTaskDetails);
         });
     }
 
--- app/src/main/java/com/rocio/taskmaster/activities/UserProfileActivity.java
+++ app/src/main/java/com/rocio/taskmaster/activities/UserProfileActivity.java
@@ -2,15 +2,47 @@ package com.rocio.taskmaster.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.rocio.taskmaster.R;
 
 public class UserProfileActivity extends AppCompatActivity {
+    public static final String USERNAME_TAG = "userName";// At top to be accessed elsewhere
+    SharedPreferences preferences; //Place near top of class
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        setupUserNameEditText();
+        setupSaveButton();
+    }
+
+    void setupUserNameEditText(){
+       String userName = preferences.getString(USERNAME_TAG, null);
+        ((EditText)findViewById(R.id.UserProfileActivityUserNameInput)).setText(userName);
+    }
+
+    void setupSaveButton() {
+        ((Button)findViewById(R.id.UserProfileActivitySaveBtn)).setOnClickListener(view -> {
+            SharedPreferences.Editor preferencesEditor = preferences.edit();
+            EditText userNameEditText = (EditText) findViewById(R.id.UserProfileActivityUserNameInput);
+            String userNameString = userNameEditText.getText().toString();
+
+            preferencesEditor.putString(USERNAME_TAG, userNameString);
+            preferencesEditor.apply();// Nothing will save without this line
+                //Can use either option to confirm save
+//            Snackbar.make(findViewById(R.id.userProfileActivityView), "Username saved!", Snackbar.LENGTH_SHORT).show();
+            Toast.makeText(UserProfileActivity.this, "Username saved!", Toast.LENGTH_SHORT).show();
+        });
     }
 }
